7af88888961e116313f9720fbb1748b202974576
govind <govind@gmail.com>
2025-06-04 20:26:49 +0530

    homepage is created

---
+++ src/homepage/index.jsx
@@ -0,0 +1,17 @@
+import './index.css'
+
+const HomePage=()=>{
+    return(
+        <div className="main">
+            <div className="main2">
+                <h1 className='head'>Welcome to PopX</h1>
+                <p>Lorem ipsum dolor sit amet,</p>
+                <p>consectetur adipiscing elit,</p>
+                <div><button className='b1'>Create Account</button></div>
+                <div> <button className='b2'>Already Registered?Login</button></div>
+                </div>
+        </div>
+    )
+}
+
+export default HomePage;
